81a04851d03aa471c38993b4b2ac6e67a1eb1545
Dongmin Shin <newdm2000@gmail.com>
2022-01-13 16:58:37 +0900

    2022-01-13 16:59

--- week1/exercise.js
+++ week1/exercise.js
@@ -1,13 +1,13 @@
+var name;  //name이라는 변수를 선언한다.
+name = "그랩"; //name이라고 선언된 변수에 "그랩"이라는 값을 저장한다.
+var name = "그랩"; 
 
-var product = [{
-    name = "keyboard",
-    price = 50000
-}, {
-    name = "mouse",
-    price =20000
-}, {
-    name = "monitor",
-    price = 100000
-}]
+var num1 = 20;
+var num1 = 'Bob';
 
-product[1].name
+let num2 = 20;
+num2 = "hello";
+let num2 = 'Bob'; // <- 에러!
+
+const num3 = 30;
+num3 = 'Bob'; // <- 에러!
